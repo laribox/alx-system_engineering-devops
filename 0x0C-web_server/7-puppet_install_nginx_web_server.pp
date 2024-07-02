ff2df567b1ee12709a98ea1e71d581cf6403f83f
laribox <laribox2014@gmail.com>
2024-07-02 08:43:00 -0700

    task 4 answer

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,18 +3,18 @@ package { 'nginx':
   ensure => installed,
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-}
-
 file_line { 'redirect_me':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.google.com permanent;',
 }
 
 file { '/var/www/html/index.html':
   content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
